2ccb0a1a33eaf6800f85e855cacdb56ca123a5ae
Joshua Ramat <ramatjoshua@gmail.com>
2022-11-19 12:31:23 -0800

    added functionality to count words in text area and added an about page on my own

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage),
-    path('count/', views.count, name="count")
+    path('', views.homepage, name="home"),
+    path('count/', views.count, name="count"),
+    path('about/', views.about, name="about")
 ]
